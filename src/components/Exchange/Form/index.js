b9bafc643b6e49a96066d7f94ccb8c882888dc91
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-24 01:02:13 +0300

    Add react hook form and add data from api to selects

--- src/components/Exchange/Form/index.js
+++ src/components/Exchange/Form/index.js
@@ -1,11 +1,34 @@
 import React, { useContext } from 'react'
 import { MainContext } from '../../../context';
+import { useForm } from "react-hook-form";
 
 function Index() {
   const { symbols, setSymbols } = useContext(MainContext)
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
 
   return (
-    <div></div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <select>
+        {
+          symbols.map((symbol, i) => (
+            <option key={i}>
+              {symbol.symbol}
+            </option>
+          ))
+        }
+      </select>
+      <select>
+        {
+          symbols.map((symbol, i) => (
+            <option key={i}>
+              {symbol.symbol}
+            </option>
+          ))
+        }
+      </select>
+      <button type="submit">Create</button>
+    </form>
   )
 }
 
