8882ee6dc54eb064ed285a93b31fffef7db2b622
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-24 21:42:18 +0300

    Add the values enter from the exchange to the table

--- src/components/Exchange/List/index.js
+++ src/components/Exchange/List/index.js
@@ -2,17 +2,32 @@ import React, { useContext } from 'react'
 import { MainContext } from '../../../context';
 
 function Index() {
-  const { symbols } = useContext(MainContext)
+  const { exchanges } = useContext(MainContext)
 
   return (
-    <div>
-      {/* {
-        symbols.map((symbol, i) => (
-          <div key={i}>
-            {symbol.symbol} = {symbol.currency}
-          </div>
-        ))
-      } */}
+    <div className="mt-20 overflow-x-auto relative shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <tbody>
+          <tr>
+            <th>To</th>
+            <th>From</th>
+            <th>Amount</th>
+            <th>Result</th>
+          </tr>
+        </tbody>
+        <tbody>
+          {
+            exchanges.map((exchange, i) => (
+              <tr key={i}>
+                <td>{exchange.to}</td>
+                <td>{exchange.from}</td>
+                <td>{exchange.amount}</td>
+                <td>{exchange.result}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
