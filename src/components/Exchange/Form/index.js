fc96b1756bd8afad7f4264dc4f2d657d78647550
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-25 15:00:16 +0300

    Change the validation structure of the amount

--- src/components/Exchange/Form/index.js
+++ src/components/Exchange/Form/index.js
@@ -23,7 +23,7 @@ function Index() {
   }
 
   return (
-    <form className="mt-60 max-w-xl p-6 mx-auto bg-white rounded-md dark:bg-gray-800"
+    <form className="mt-40 max-w-xl p-6 mx-auto bg-white rounded-md dark:bg-gray-800"
       onSubmit={handleSubmit(onSubmit)}>
       <select className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
         {...register("to", { required: true })}>
@@ -50,7 +50,14 @@ function Index() {
       <input className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
         placeholder='Amount'
         type='number'
-        {...register("amount", { required: true, })}>
+        {...register("amount", {
+          required: true,
+          valueAsNumber: true,
+          validate: (value) => value > 0,
+          pattern: {
+            value: /^(0|[1-9]\d*)(\.\d+)?$/
+          },
+        })}>
       </input>
       <button className='mt-5 float-right bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-4 border border-gray-700 rounded'
         type="submit">Create</button>
