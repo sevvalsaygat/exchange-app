53756191ece0767647cff86beb60b99707b9b190
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-25 15:08:24 +0300

    Delete type of amount

--- src/components/Exchange/Form/index.js
+++ src/components/Exchange/Form/index.js
@@ -49,7 +49,6 @@ function Index() {
       </select>
       <input className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring'
         placeholder='Amount'
-        type='number'
         {...register("amount", {
           required: true,
           valueAsNumber: true,
